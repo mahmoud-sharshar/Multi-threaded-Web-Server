bab04bb0b10f20e15e00d0188b0c3a8cc638ea70
mahmoud hamed <mahmoud.hamed@blink22.com>
2020-11-28 06:47:51 +0200

    Cofigure the server to be multithreaded and Add Get, Post requests and responses

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -6,42 +6,175 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 #define SERVER_PORT 5100
-using namespace std;
-void dieWithError(string errorMessage); //error handling function
+#define GET "GET"
+#define POST "POST"
+#define BUFF_SIZE 10000
 
-int main()
+using namespace std;
+
+struct Request
 {
-	int sock;
-	struct sockaddr_in serverAddr;	
-	unsigned short serverPort = SERVER_PORT;
-	//create a reliable stream socket using TCP
-	if((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
-		dieWithError("socket() failed");
+	string method;
+	string URI;
+	string hostName;
+	int port = 80;
+};
+
+void dieWithError(string errorMessage); //error handling function
+Request parseCommand(string command);
+vector<Request> parseCommandsFile(string file_path);
+string buildRequestMessage(Request request);
+
+void error_msg(const char *msg, bool halt_flag)
+{
+	perror(msg);
+	if (halt_flag)
+		exit(-1);
+}
+
+int main(int argc, char *argv[])
+{
+	// Check the number of parameters
+	if (argc < 3)
+	{
+		// Tell the user how to run the program
+		cerr << "Usage: " << argv[0] << " server_name port_number" << endl;
+		return 1;
 	}
+
+	int sock;
+	struct sockaddr_in serverAddr;
+	unsigned short serverPort = stoi(argv[2]);
 
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(serverPort);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	// serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serverAddr.sin_addr.s_addr = *((unsigned long *)gethostbyname(argv[1])->h_addr_list[0]);
 
-	//establish connection to the server
-	if(connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0){
-		dieWithError("connect() failed");
+	vector<Request> requests = parseCommandsFile("../store/commands.txt");
+
+	for (int i = 0; i < requests.size(); ++i)
+	{
+		//create a reliable stream socket using TCP
+		if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		{
+			dieWithError("socket() failed");
+		}
+
+		if (connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+		{
+			dieWithError("connect() failed");
+		}
+		string requestMsg = buildRequestMessage(requests[i]);
+		char *message = new char[requestMsg.size() + 1];
+		strcpy(message, requestMsg.c_str());
+		int msgLen = strlen(message);
+		if (send(sock, message, msgLen, 0) != msgLen)
+		{
+			dieWithError("send() sent a different number of bytes than expected");
+		}
+		char buffer[BUFF_SIZE + 1];
+		bzero(buffer, sizeof(buffer));
+		int bytes_read = recv(sock, buffer, sizeof(buffer), 0);
+		if (bytes_read < 0)
+			error_msg("Problem with recv call", false);
+		cout << "##########################################" << endl;
+		cout << "Response : \n";
+		cout << "----------------" << endl;
+		cout << buffer;
+		cout << "##########################################" << endl;
+		// cout << "connection successed\n";
+		close(sock);
 	}
-	char* message = "Hello world!";
-	int msgLen = strlen(message);
-	if(send(sock , message , msgLen , 0 ) != msgLen){
-		dieWithError("send() sent a different number of bytes than expected");
-	}
-	cout << "connection successed";
-	close(sock);
 	return 0;
+}
+
+vector<Request> parseCommandsFile(string file_path)
+{
+	string command;
+	vector<Request> requests;
+	// Read from the text file
+	ifstream commandsFile(file_path);
+
+	// Use a while loop together with the getline() function to read the file line by line
+	while (getline(commandsFile, command))
+	{
+		// Output the text from the file
+		requests.push_back(parseCommand(command));
+	}
+	return requests;
+}
+
+Request parseCommand(string command)
+{
+	istringstream ss(command);
+	vector<string> commandParts;
+	string buf;
+	while (ss >> buf)
+		commandParts.push_back(buf);
+	Request new_request;
+	if (commandParts.size() > 2)
+	{
+		if (commandParts[0] == "client_get")
+		{
+			new_request.method = GET;
+		}
+		else if (commandParts[0] == "client_post")
+		{
+			new_request.method = POST;
+		}
+		new_request.URI = commandParts[1];
+		new_request.hostName = commandParts[2];
+		if (commandParts.size() > 3)
+		{
+			new_request.port = stoi(commandParts[3]);
+		}
+	}
+	return new_request;
+}
+
+// build HTTP request message
+string buildRequestMessage(Request request)
+{
+	string message = "";
+	if (request.method == GET)
+	{
+		message += GET;
+		message += (' ' + request.URI + " HTTP/1.1\n"); // request header
+		message += "\n";
+	}
+	else if (request.method == POST)
+	{
+		message += POST;
+		message += (' ' + request.URI + " HTTP/1.1\n"); // request header
+		message += "\n";
+		ifstream file("../store" + request.URI);
+		string line;
+		while (getline(file, line))
+		{
+			message += line;
+			message += "\n";
+		}
+	}
+	cout << "##########################################" << endl;
+	cout << "Sending new Request message: \n";
+	cout << "---------------------------" << endl;
+	cout << message;
+	cout << "##########################################" << endl;
+	return message;
 }
 
 void dieWithError(string errorMessage)
 {
 	// fprintf(stderr, "%s\n", errorMessage);
-	cout << errorMessage;
+	cout << errorMessage << endl;
 	exit(1);
 }
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,118 +4,265 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define PORT 5100
 #define MAXPENDING 5
-#define RECVBUFSIZE 100
+#define BUFF_SIZE 10000
 
 using namespace std;
+
+struct ClientRequest
+{
+	string method;
+	string filePath;
+	string protocol;
+	vector<string> data;
+};
+
 void dieWithError(string errorMessage); //error handling function
 void handleTCPClient(int clientSocket);
+ClientRequest parseRequestLine(string line);
+ClientRequest parseRequestMessage(string message);
+string buildResponseMessage(string status_code, string status_response, string filePathData = "");
+string handleGetRequest(ClientRequest request);
+string handlePostRequest(ClientRequest request);
+void writeDataToFile(vector<string> data, string filePath);
 
-int main()
+void error_msg(const char *msg, bool halt_flag)
 {
-	// int sockid= socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	// cout << sockid << endl;
-	// int status= close(sockid);
-	// cout << status << endl;
-	int serverSocket;	//Socket descriptor for server
-	int clientSocket;	//Socket descriptor for client
+	perror(msg);
+	if (halt_flag)
+		exit(-1);
+}
 
-	struct sockaddr_in serverAddr;	//local address
-	struct sockaddr_in clientAddr;	//client address
+void *handleTCPClient(void *client_ptr)
+{
+	pthread_detach(pthread_self()); /* terminates on return */
+	// cout << "starting new thread" << endl;
+	/* read/write socket */
+	int client = *((int *)client_ptr);
 
-	unsigned short serverPort;	//server port
-	unsigned int clientLen;			//length of client address data structure
+	/* request */
+	char buffer[BUFF_SIZE + 1];
+	bzero(buffer, sizeof(buffer));
+	int bytes_read = recv(client, buffer, sizeof(buffer), 0);
+	if (bytes_read < 0)
+		error_msg("Problem with recv call", false);
 
+	ClientRequest request = parseRequestMessage(buffer);
+	string response;
+	if (request.method == "GET")
+	{
+		response = handleGetRequest(request);
+	}
+	else if (request.method == "POST")
+	{
+		response = handlePostRequest(request);
+	}
+	/* response */
+	// bzero(response, sizeof(response));
+	// generate_echo_response(buffer, response);
+	int bytes_written = send(client, response.c_str(), response.size(), 0);
+	if (bytes_written < 0)
+		error_msg("Problem with send call", false);
 
-	
+	close(client);
+
+	return NULL;
+} /* detached thread terminates on return */
+
+int main(int argc, char* argv[])
+{
+	 // Check the number of parameters
+    if (argc < 2) {
+        // Tell the user how to run the program
+        cerr << "Usage: " << argv[0] << " port-number" << endl;
+        return 1;
+    }
+
+	int serverSocket; //Socket descriptor for server
+	int clientSocket; //Socket descriptor for client
+
+	struct sockaddr_in serverAddr; //local address
+	struct sockaddr_in clientAddr; //client address
+
+	unsigned short serverPort = stoi(argv[1]); //server port
+	unsigned int clientLen;	   //length of client address data structure
+
 	//create socket for incoming connections
-	if((serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
+	if ((serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
 		dieWithError("socket() failed");
 	}
-	
+
 	//construct local address structure
-	serverPort = PORT;	//server port
+	// serverPort = PORT; //server port
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(serverPort);
 	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	//bind to the local address
-	if( bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0){
+	if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+	{
 		dieWithError("bind() failed");
 	}
 
 	//mark the socket so it will listen for incoming connections
-	if(listen(serverSocket, MAXPENDING) < 0){
+	if (listen(serverSocket, MAXPENDING) < 0)
+	{
 		dieWithError("listen() failed");
 	}
 
-	while(true){
+	cout << argv[0] << " is listening on port " << argv[1] << "!" << endl;
+
+	while (true)
+	{
 		//set the size of the in-out parameter
 		clientLen = sizeof(clientAddr);
 		cout << "Ready to accept" << endl;
 		//wait for client to connect
-		if((clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientLen)) < 0){
+		if ((clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientLen)) < 0)
+		{
 			dieWithError("accept() failed");
 		}
-		cout << "finish accept" << endl;
-		cout << clientSocket << endl;
-		// close(clientSocket);
 		//clientSocket is connected to a client
-		printf("Handling client %s\n", inet_ntoa(clientAddr.sin_addr));
-		handleTCPClient(clientSocket);
+		cout << "----------------------------" << endl;
+		printf("Accepting connection from client %s with socket %d\n", inet_ntoa(clientAddr.sin_addr), clientSocket);
+		cout << "----------------------------" << endl;
+		pthread_t tid;
+		int flag = pthread_create(&tid,			   /* id */
+								  NULL,			   /* attributes */
+								  handleTCPClient, /* routine */
+								  &clientSocket);  /* routine's arg */
+		if (flag < 0)
+			error_msg("Problem creating thread", false);
 	}
+}
+
+bool isFileExists(string filePath)
+{
+	ifstream f(filePath);
+	bool status = f.good();
+	f.close();
+	return status;
+}
+
+string handleGetRequest(ClientRequest request)
+{
+	string response;
+	// cout << "file Path: " << request.filePath << endl;
+	string filePath = "../store" + request.filePath;
+	if (isFileExists(filePath))
+	{
+		response = buildResponseMessage("200", "OK", filePath);
+	}
+	else
+	{
+		response = buildResponseMessage("404", "Not Found");
+	}
+	return response;
+}
+
+string handlePostRequest(ClientRequest request)
+{
+	string response;
+	writeDataToFile(request.data, "../store" + request.filePath);
+	response = buildResponseMessage("200", "OK");
+	return response;
+}
+
+void writeDataToFile(vector<string> data, string filePath)
+{
+	ofstream file(filePath);
+
+	// Write to the file
+	for (auto line : data)
+	{
+		file << line;
+		file << "\n";
+	}
+	// Close the file
+	file.close();
+}
+
+ClientRequest parseRequestMessage(string message)
+{
+	cout << "##########################################" << endl;
+	cout << "New Request comming!" << endl;
+	cout << "----------------------------" << endl;
+	cout << message;
+	cout << "##########################################" << endl;
+	istringstream f(message);
+	string line;
+	getline(f, line);
+	ClientRequest newRequest = parseRequestLine(line);
+	bool headerFinished = false;
+	while (getline(f, line))
+	{
+		if (headerFinished)
+			newRequest.data.push_back(line);
+
+		if (line.empty())
+		{
+			headerFinished = true;
+		}
+	}
+	return newRequest;
+}
+
+string buildResponseMessage(string status_code, string status_response, string filePathData)
+{
+	string response = "HTTP/1.1 " + status_code + ' ' + status_response + "\n\n";
+	if (filePathData != "")
+	{
+		string line;
+		ifstream dataFile(filePathData);
+
+		while (getline(dataFile, line))
+		{
+			response += (line + "\n");
+		}
+		dataFile.close();
+	}
+	cout << "##########################################" << endl;
+	cout << "Response message: \n";
+	cout << "----------------------------" << endl;
+	cout << response;
+	cout << "##########################################" << endl;
+	return response;
+}
+
+ClientRequest parseRequestLine(string line)
+{
+	// cout << "Request Line: " << line << endl;
+	istringstream R(line);
+	string part;
+	vector<string> requestParts;
+	while (getline(R, part, ' '))
+	{
+		requestParts.push_back(part);
+		// cout << part << endl;
+	}
+	ClientRequest newRequest;
+	if (requestParts.size() > 2)
+	{
+		newRequest.method = requestParts[0];
+		newRequest.filePath = requestParts[1];
+		newRequest.protocol = requestParts[2];
+	}
+	return newRequest;
 }
 
 void dieWithError(string errorMessage)
 {
-	// fprintf(stderr, "%s\n", errorMessage);
-	cout << errorMessage;
+	cout << errorMessage << endl;
 	exit(1);
-}
-
-void handleTCPClient(int clientSocket)
-{
-	char echoBuffer[RECVBUFSIZE];
-	int msgSize;
-	// char dle_etx[] = "\x10\x03";
-	int sent;
-	//recieve message from client
-	int count = 0;
-
-	// while(1)
-	// {
-				//see if there is more data to recieve
-		if((msgSize = recv(clientSocket, echoBuffer, RECVBUFSIZE, 0)) < 0){
-			dieWithError("recv() failed");
-		}
-		echoBuffer[msgSize] = '\0';
-		printf("recved %s\n", echoBuffer);
-
-
-
-	// 	if((sent = send(clientSocket, echoBuffer, msgSize, 0) )!= msgSize){
-	// 		printf("ms = %d, but sent %d\n",msgSize, sent );
-	// 		dieWithError("send() failed");
-	// 	}
-
-	// 	if(msgSize ==  0){
-	// 		break;
-	// 	}
-	// 	if(strcmp(dle_etx, echoBuffer) == 0){
-	// 		break;
-	// 	}
-	// 	count++;
-
-	// }
-	// printf("EnhancedServer echoed %d strings.\n",count);
-	
-	//recieve last packet to finish
-	// recv(clientSocket, echoBuffer, RECVBUFSIZE, 0);
-
-
-	close(clientSocket);
-	// }
-
 }
